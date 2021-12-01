c2dd239ba8e2e9e92da2b070e598d0a741e74b7a
naerim <naerim1119@gmail.com>
2021-12-01 23:55:08 +0900

    타입스크립트 기본 타입

---
+++ 1-TYPES/1-1-basic.ts
@@ -0,0 +1,74 @@
+{
+    // javaScript
+    // old: var 💩
+    var age1 = 5;
+    age1 = 1;
+
+    // let es6
+    let name1 = 'hello';
+    name1 = 'hi';
+
+    // const
+    const age2 = 5;
+    // age2 = 5; error
+
+    /**
+     * JavaScript
+     * Primitive: number, string, boolean, bigint, symbol, null, undefined
+     * Object: function, array....
+     */
+
+    // number
+    const num:number = -6;
+
+    // string
+    const str:string = 'hello';
+
+    // boolean
+    const boal:boolean = true;
+    
+    // undefined
+    let name: undefined; // 💩
+    let age: number | undefined;
+    age = undefined;
+    age = 1;
+    function find(): number | undefined {
+        return undefined;
+    }
+
+    // null
+    let person = null; // 💩
+    let person2: string | null;
+
+    // unknown 💩
+    let notSure: unknown = 0;
+    notSure = 'he';
+    notSure = true;
+
+    // any 💩
+    let anything: any = 0;
+    anything = 'hello';
+
+    // void
+    function print(): void {
+        console.log('hello');
+        return ;
+    }
+    let unusable: void = undefined; // 💩
+
+    // never
+    function throwError(message: string): never {
+        // message -> server (log)
+        throw new Error(message);
+        /*
+        while(true) {}
+        */
+    }
+    let neverEnding: never; // 💩
+
+    // object
+    let obj: object; // 💩
+    function acceptSomeObject(obj: object) {}
+    acceptSomeObject({ name: 'ellie'});
+    acceptSomeObject({ animal: 'dog'});
+}
