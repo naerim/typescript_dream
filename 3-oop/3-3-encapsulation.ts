a566cf0d01bd52cb8da9a406ee00b6988223f1dd
naerim <naerim1119@gmail.com>
2021-12-12 13:21:04 +0900

    getter, setter

--- 3-oop/3-3-encapsulation.ts
+++ 3-oop/3-3-encapsulation.ts
@@ -40,4 +40,23 @@
 
     const maker = CoffeeMaker.makeMachine(32);
     maker.fillCoffeeBeans(32);
+
+    class User {
+        get fullName(): string {
+            return`${this.firstName} ${this.lastName}`;
+        }
+        private internalAge = 4;
+        get age(): number {
+            return this.internalAge;
+        }
+        set age(num: number) {
+            this.internalAge = num;
+        }
+        constructor(private firstName: string, public lastName: string) {
+        }
+    }
+    const user = new User('Steve', 'Jobs');
+    console.log(user.fullName);
+    user.age = 6;
+    console.log(user.fullName);
 }
