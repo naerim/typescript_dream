effe7091818db433bef4ce243d5e3e6a10fea617
naerim <naerim1119@gmail.com>
2021-12-30 20:09:25 +0900

    stack 구현

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -4,16 +4,45 @@ interface Stack {
   pop(): string;
 }
 
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+};
+
 class StackImpl implements Stack {
-  private _size: number;
+  private _size: number = 0;
+  private head?: StackNode;
+  constructor(private capacity: number) {}
   get size() {
     return this._size;
   }
   push(value: string) {
+    if (this.size === this.capacity) {
+      throw new Error("Stack is full!");
+    }
+    const node: StackNode = { value, next: this.head };
+    this.head = node;
     this._size++;
   }
 
   pop(): string {
+    // null == undefined, null !-- undefined
+    if (this.head == null) {
+      throw new Error("Stack is empty!");
+    }
+    const node = this.head;
+    this.head = node.next;
     this._size--;
+    return node.value;
   }
 }
+
+const stack = new StackImpl(10);
+stack.push("Naerim 1");
+stack.push("Naerim 2");
+stack.push("Naerim 3");
+while (stack.size !== 0) {
+  console.log(stack.pop());
+}
+
+// stack.pop();
